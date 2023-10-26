1048455ed4103b31e790559a5a8f6d750d9682af
laridacosta <149098435+laridacosta@users.noreply.github.com>
2023-10-26 15:21:50 -0300

    Add files via upload

---
+++ bookstore.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 26-Out-2023 às 20:20
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `gerente`
+--
+
+CREATE TABLE `gerente` (
+  `id_gerente` int(11) NOT NULL,
+  `user` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `id_livros` int(11) NOT NULL,
+  `imagens` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(200) NOT NULL,
+  `e-mail` varchar(200) NOT NULL,
+  `senha` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `votacoes`
+--
+
+CREATE TABLE `votacoes` (
+  `id_votos` int(11) NOT NULL,
+  `id_livros` int(11) NOT NULL,
+  `votos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `votos_usu`
+--
+
+CREATE TABLE `votos_usu` (
+  `id_votosUsu` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_livro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `gerente`
+--
+ALTER TABLE `gerente`
+  ADD PRIMARY KEY (`id_gerente`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`id_livros`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Índices para tabela `votacoes`
+--
+ALTER TABLE `votacoes`
+  ADD PRIMARY KEY (`id_votos`),
+  ADD KEY `id_livros` (`id_livros`);
+
+--
+-- Índices para tabela `votos_usu`
+--
+ALTER TABLE `votos_usu`
+  ADD PRIMARY KEY (`id_votosUsu`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `gerente`
+--
+ALTER TABLE `gerente`
+  MODIFY `id_gerente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `livros`
+--
+ALTER TABLE `livros`
+  MODIFY `id_livros` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `votacoes`
+--
+ALTER TABLE `votacoes`
+  MODIFY `id_votos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `votos_usu`
+--
+ALTER TABLE `votos_usu`
+  MODIFY `id_votosUsu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `votacoes`
+--
+ALTER TABLE `votacoes`
+  ADD CONSTRAINT `votacoes_ibfk_1` FOREIGN KEY (`id_livros`) REFERENCES `livros` (`id_livros`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
