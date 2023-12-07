424de9ab21fa460bd3f7dc0d7321e591e237a509
laridacosta <149098435+laridacosta@users.noreply.github.com>
2023-12-07 14:01:01 -0300

    Delete BookFlirt-main/src directory

--- BookFlirt-main/src/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Iniciando a sessão:
-if (session_status() !== PHP_SESSION_ACTIVE) {
-  session_start();
-}
-//Apagando todos os dados de uma sessão:
-session_unset();
-header("Location: ../index.html");
